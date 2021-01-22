07ca2b6b305135675f3474b799d47cbb2f0d1de4
kiadimalala <59082037+kiadimalala@users.noreply.github.com>
2021-01-22 09:01:12 +0300

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3ed465136a528143af7a39ef92152886'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '85d73cff-05cb-48a4-9525-f83d2e3cc819'
+};
